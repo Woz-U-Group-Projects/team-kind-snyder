082acd0e93f268d5c0a0e5cd9ae84ab7ca49fe3d
Luis Lopez <delucholuis@gmail.com>
2019-09-24 13:17:04 -0700

    created jwt and entity

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
+import { RouterModule } from '@angular/router';
 import { AppComponent } from "./app.component";
 import { ProjectDisplayComponent } from './project-display/project-display.component';
+import { SnapDayComponent } from './components/snap-day/snap-day.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, ProjectDisplayComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, ProjectDisplayComponent, SnapDayComponent, HomeComponent, SignupComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])],
   providers: [],
   bootstrap: [AppComponent]
 })
